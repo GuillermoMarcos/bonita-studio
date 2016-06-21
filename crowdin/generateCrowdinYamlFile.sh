85b0a162785eb29cfd1a8045d00e900d0f53a9bb
Romain Bioteau <romain.bioteau@gmail.com>
2016-06-21 11:40:43 +0200

    refactor crowdin

---
+++ crowdin/generateCrowdinYamlFile.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+#
+# args
+# 1 : project api key
+# 2 : path of the source to parse
+
+
+. ./utils/utils.sh
+
+if test ! $# -eq 2  
+then
+	echo "[ERROR] generateCrowdinYamlFile.sh scripts needs 2 parameters"
+	exit
+fi
+
+projectAPIKey=$1
+sourcePath=$2
+
+crowdinFile=crowdin.yaml
+crowdinTemplateFile=crowdin/crowdin.yaml.template
+
+echo "[INFO] Remove Crowdin configuration file if exists."
+# remove crowdin config file if already exists
+if test -f $crowdinFile
+then
+	echo " remove file $crowdinFile"
+	rm $crowdinFile
+fi
+
+echo "[INFO] Create Crowdin configuration file."
+# copy template
+cp $crowdinTemplateFile $crowdinFile
+
+# Set config
+sed -i "s#@PROJECT_API_KEY@#$projectAPIKey#g" $crowdinFile
+sed -i "s#@PATH_ON_CI@#$sourcePath#g"         $crowdinFile
+
+
